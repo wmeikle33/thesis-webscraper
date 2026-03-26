28a8cbdad802a180e55a1ca9a2631cd64b274d05
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-27 01:19:26 +0800

    Update __init__.py

--- src/thesis_webscraper/__init__.py
+++ src/thesis_webscraper/__init__.py
@@ -1,7 +1,7 @@
 
-from .models import Post
+from .models import ScrapeResult
 from .parser import parse_list, parse_post
 
 __version__ = "0.1.0"
 
-__all__ = ["Post", "parse_list", "parse_post"]
+__all__ = ["ScrapeResult", "parse_list", "parse_post"]
