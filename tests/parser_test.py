0c23947bca34a7f6c2dbe6763b36bcbb36a7fc43
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-29 21:09:38 +0800

    Update parser_test.py

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,1 +1,14 @@
+def test_parse_valid_csv():
+    csv_content = "header1,header2\nvalue1,value2\nvalue3,value4"
+    file_handle = io.StringIO(csv_content)
+    
+    expected_data = [
+        ['header1', 'header2'],
+        ['value1', 'value2'],
+        ['value3', 'value4']
+    ]
+  
+    actual_data = parse_csv_data(file_handle)
+    
+    assert actual_data == expected_data
 
